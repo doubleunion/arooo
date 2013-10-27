c6c188084d94ca92c30043a0db47f108d7e34753
Heather Rivers <heather@heatherrivers.com>
2013-10-27 15:19:05 -0700

    Use a container div by default but allow override for jumbotron

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,35 +1,45 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  helper_method :current_user
-  helper_method :user_signed_in?
-  helper_method :correct_user?
+  helper_method :current_user, :user_signed_in?, :correct_user?,
+    :use_container?
+
+  protected
+
+  def set_use_container(bool)
+    @_use_container = bool
+  end
 
   private
-    def current_user
-      begin
-        @current_user ||= User.find(session[:user_id]) if session[:user_id]
-      rescue Exception => e
-        nil
-      end
-    end
 
-    def user_signed_in?
-      return true if current_user
+  def current_user
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue Exception => e
+      nil
     end
+  end
 
-    def correct_user?
-      @user = User.find(params[:id])
-      unless current_user == @user
-        redirect_to root_url, :alert => "Access denied."
-      end
+  def user_signed_in?
+    return true if current_user
+  end
+
+  def correct_user?
+    @user = User.find(params[:id])
+    unless current_user == @user
+      redirect_to root_url, :alert => "Access denied."
     end
+  end
 
-    def authenticate_user!
-      if !current_user
-        redirect_to root_url, :alert => 'You need to sign in for access to this page.'
-      end
+  def authenticate_user!
+    if !current_user
+      redirect_to root_url, :alert => 'You need to sign in for access to this page.'
     end
+  end
 
+  def use_container?
+    return @_use_container if defined?(@_use_container)
+    @_use_container = true
+  end
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
