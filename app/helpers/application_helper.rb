c1d4561d5eccd81252a2519a5be213edcfb972a3
Heather Rivers <heather@heatherrivers.com>
2013-11-10 21:49:36 -0800

    Add google analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,22 @@ module ApplicationHelper
   def external_link_to(label, url, opts = {})
     link_to(label, url, { :target => '_blank' }.merge(opts))
   end
+
+  def google_analytics
+    return unless Rails.env.production?
+
+    code = <<-eos
+      <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', '#{GOOGLE_ANALYTICS_ID}']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+      </script>
+    eos
+    code.html_safe
+  end
 end
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -5,3 +5,4 @@ TUMBLR_BASE          = 'doubleunion.tumblr.com'
 PAYPAL_EMAIL         = 'paypal@doubleunion.org'
 CONTACT_EMAIL        = 'doubleunionsf@gmail.com'
 MAILING_LIST_GENERAL = "http://lists.doubleunion.org/listinfo.cgi/doubleunion-doubleunion.org"
+GOOGLE_ANALYTICS_ID  = 'UA-371951-49'
