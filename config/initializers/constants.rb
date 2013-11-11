efa0ffbede6165d910b1882b6a008bb33e63e56a
Heather Rivers <heather@heatherrivers.com>
2013-11-10 18:15:04 -0800

    Stylistic tweaks

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,7 @@
-TWITTER_URL      = 'https://twitter.com/doubleunionsf'
-TWITTER_USERNAME = 'doubleunionsf'
-PRESS_EMAIL      = 'press@doubleunion.org'
-TUMBLR_BASE      = 'doubleunion.tumblr.com'
-PAYPAL_EMAIL     = 'paypal@doubleunion.org'
-CONTACT_EMAIL    = 'doubleunionsf@gmail.com'
+TWITTER_URL          = 'https://twitter.com/doubleunionsf'
+TWITTER_USERNAME     = 'doubleunionsf'
+PRESS_EMAIL          = 'press@doubleunion.org'
+TUMBLR_BASE          = 'doubleunion.tumblr.com'
+PAYPAL_EMAIL         = 'paypal@doubleunion.org'
+CONTACT_EMAIL        = 'doubleunionsf@gmail.com'
+MAILING_LIST_GENERAL = "http://lists.doubleunion.org/listinfo.cgi/doubleunion-doubleunion.org"
