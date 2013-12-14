fc1d4ecd12d016e437dbb83afb7172205d5ee849
Amelia Greenhall <amelia.greenhall@gmail.com>
2013-12-14 10:29:34 -0800

    fixes issue #4 making links on applications pages (and also on member profile pages) open in a new window

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,10 @@ module ApplicationHelper
   def external_link_to(label, url, opts = {})
     link_to(label, url, { :target => '_blank' }.merge(opts))
   end
+  
+  def external_auto_link(url)
+    auto_link(url, :html => { :target => '_blank' })
+  end
 
   def s3_url(name)
     "#{S3_BUCKET}/#{name}"
