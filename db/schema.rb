956254dc96fd6eac8fae0884ecbc31f3d656d565
Lillie Chilen <lillie.chilen@gmail.com>
2014-01-08 08:48:32 -0800

    Add feminism essay question to application

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131229225142) do
+ActiveRecord::Schema.define(version: 20140108163939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20131229225142) do
     t.string   "reasons",           limit: 2000
     t.string   "projects",          limit: 2000
     t.string   "skills",            limit: 2000
+    t.string   "feminism",          limit: 2000
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
