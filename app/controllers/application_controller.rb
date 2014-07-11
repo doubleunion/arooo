fdac0768c020b75d8454936f465860b75655d0ca
Lillie Chilen <lillie.chilen@gmail.com>
2014-07-10 20:10:35 -0700

    Don't send an email to membership coordinators when the voting threshold is passed
    - They are doing acceptance in waves this time around, so this isn't a helpful email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -87,6 +87,8 @@ class ApplicationController < ActionController::Base
 
     vote.save!
 
-    application.votes_threshold_email
+    # current process makes the votes threshold email not helpful
+    # so, for now, no email. maybe this should be deleted in the future
+    #application.votes_threshold_email
   end
 end
