739b9ecf998f66415e5acc3073fe171e993d0673
Lillie Chilen <lillie.chilen@gmail.com>
2014-11-09 21:55:00 -0800

    Update to modern shoulda-matchers 'validate_inclusion_of' syntax

--- spec/models/authentication_spec.rb
+++ spec/models/authentication_spec.rb
@@ -7,7 +7,7 @@ describe Authentication do
     it { should validate_presence_of :user }
     it { should validate_presence_of :uid }
     it { should validate_presence_of :provider }
-    it { should ensure_inclusion_of(:provider).in_array(["github"])}
+    it { should validate_inclusion_of(:provider).in_array(["github"])}
     it { should validate_uniqueness_of(:uid).scoped_to(:user_id)}
   end
 end
