c8364855b0616eac2ba5f3abe442d0772aada1cc
Linda Goldstein <lindag@squareup.com>
2019-06-06 22:13:10 -0700

    Set StripeEvent.signing_secret so that webhooks will work

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,12 @@
 Rails.configuration.stripe = {
+  event_signing_secret: ENV['STRIPE_SIGNING_SECRET'],
   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
   secret_key:      ENV['STRIPE_SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+StripeEvent.signing_secret = Rails.configuration.stripe[:event_signing_secret]
 
 StripeEvent.configure do |events|
   events.subscribe 'charge.succeeded', StripeEventHelper::ChargeSucceeded.new
