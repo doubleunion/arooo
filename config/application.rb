b3f81018cde60be5ae1cbc6f1a2f1f7bdaf30cfb
Linda <linda@semprehealth.com>
2020-05-06 21:53:52 -0700

    rake rails:update with all overrides, plus some undos

--- config/application.rb
+++ config/application.rb
@@ -1,26 +1,13 @@
-require File.expand_path("../boot", __FILE__)
+require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 module Doubleunion
   class Application < Rails::Application
-    # don't generate RSpec tests for views and helpers
-    config.generators do |g|
-      g.test_framework :rspec, fixture: true
-
-      g.view_specs false
-      g.helper_specs false
-    end
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -46,5 +33,8 @@ module Doubleunion
         resource "/public_members", headers: :any, methods: [:get], max_age: 0
       end
     end
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,11 @@
-filenames = (Dir["app/assets/javascripts/*"] + Dir["app/assets/stylesheets/*"]).map { |filepath|
-  File.basename(filepath).gsub(".scss", "")
-}
-Rails.application.config.assets.precompile += filenames
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
