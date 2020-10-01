ca846ac387db945bf80045a61ee002344f1d35f3
Ana Ulin <ana@ulin.org>
2020-09-30 19:46:52 -0700

    Add explicit Stripe API version to Stripe initializer.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -6,6 +6,10 @@ Rails.configuration.stripe = {
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
+# You can verify which Stripe API version is in use, or upgrade it, through the
+# Stripe developer dashboard: https://dashboard.stripe.com/developers
+Stripe.api_version = '2016-02-03'
+
 StripeEvent.signing_secret = Rails.configuration.stripe[:event_signing_secret]
 
 StripeEvent.configure do |events|
