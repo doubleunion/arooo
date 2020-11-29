c90b41695ed08ec81ac280e952e8f4d193e765e0
Britta <britta.gustafson@a1msolutions.com>
2020-11-29 15:06:49 -0800

    Content update for application questions (#521)
    * Update questions in application form itself
    
    * Add updated questions to application preview
    
    * Fix copy-paste mistake in application question formatting
    
    * Make the fields match
    
    * Add attendance field to Profile, make it save as part of the application, update spec.
    
    Co-authored-by: Ana Ulin <ana@ulin.org>

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_23_002316) do
+ActiveRecord::Schema.define(version: 2020_11_24_040111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,13 +56,12 @@ ActiveRecord::Schema.define(version: 2020_08_23_002316) do
   end
 
   create_table "door_codes", id: :serial, force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.integer "user_id"
     t.string "code", null: false
     t.boolean "enabled", default: false, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["code"], name: "index_door_codes_on_code", unique: true
-    t.index ["user_id"], name: "index_door_codes_on_user_id", unique: true
   end
 
   create_table "profiles", id: :serial, force: :cascade do |t|
@@ -82,6 +81,7 @@ ActiveRecord::Schema.define(version: 2020_08_23_002316) do
     t.string "skills", limit: 2000
     t.string "feminism", limit: 2000
     t.string "pronouns"
+    t.string "attendance", limit: 2000
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
