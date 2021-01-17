3e31b13af8f823163490b0c93bba397d2427a2a5
Ana Ulin <ana@ulin.org>
2021-01-17 15:48:23 -0800

    Redirect logged-in applicants to editing application if they hit '/'. Say "Your application" instead of "View application" when you're looking at your own application.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -12,6 +12,19 @@ describe SessionsController do
     it "renders the page" do
       expect(get(:login)).to render_template :login
     end
+
+    it "redirects member to members home" do
+      login_as(:key_member)
+      get(:login)
+      expect(response).to redirect_to members_root_path
+    end
+
+    it "redirects applicant to editing application" do
+      applicant = create(:user, state: :applicant)
+      login_as(applicant)
+      get(:login)
+      expect(response).to redirect_to edit_application_path(applicant.application)
+    end
   end
 
   describe "GET github" do
