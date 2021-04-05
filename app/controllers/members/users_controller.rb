13fa539aecf404932ff2b059cb39adf6a1c8b94e
Jenny Tong <j.tong89@gmail.com>
2021-04-04 21:11:35 -0500

    Hide joining DU reasons box unless checkbox is checked

--- app/controllers/members/users_controller.rb
+++ app/controllers/members/users_controller.rb
@@ -49,7 +49,7 @@ class Members::UsersController < Members::MembersController
   def profile_attributes
     [:id, :twitter, :facebook, :website, :linkedin, :blog,
      :summary, :reasons, :projects, :pronouns, :skills,
-     :show_name_on_site, :gravatar_email]
+     :show_name_on_site, :gravatar_email, :show_reasons]
   end
 
   def set_user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_040111) do
+ActiveRecord::Schema.define(version: 2021_04_04_213123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_040111) do
     t.string "feminism", limit: 2000
     t.string "pronouns"
     t.string "attendance", limit: 2000
+    t.boolean "show_reasons", default: false, null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
