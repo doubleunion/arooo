ced73b7ecbb1d4ba0094c99d09577319f39f4cbd
Ana Ulin <ana@ulin.org>
2022-05-31 14:49:34 -0700

    [BUG] Fix mailer configuration to match new version of aws-sdk-rails (#677)
    * Update staging and production mailer delivery method to match the values
    expected by the newer version of the aws-sdk-rails gem.
    
    * Pin aws-sdk-rails to >3. Older versions are no longer compatible with our code.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :aws_sdk
+  config.action_mailer.delivery_method = :ses
   config.action_mailer.default_url_options = {host: ENV["HOST_URL"]}
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
