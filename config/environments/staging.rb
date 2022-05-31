b672db515876c752682081885fd373bf69fb20ff
Ana Ulin <ana@ulin.org>
2022-05-31 14:36:32 -0700

    Update staging and production mailer delivery method to match the values expected by the newer version of the aws-sdk-rails gem.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -74,7 +74,7 @@ Doubleunion::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :aws_sdk
+  config.action_mailer.delivery_method = :ses
   config.action_mailer.default_url_options = {host: ENV["HOST_URL"]}
 
   # Disable automatic flushing of the log to improve performance.
