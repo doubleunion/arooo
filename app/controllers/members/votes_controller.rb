7e3a931e15095b2e8e8a39df3c15dc09236de3c5
Linda Goldstein <linda.goldstein@parachutehealth.com>
2024-02-07 16:23:26 -0800

    WIP add Not Sure to voting options

--- app/controllers/members/votes_controller.rb
+++ app/controllers/members/votes_controller.rb
@@ -1,6 +1,7 @@
 class Members::VotesController < Members::MembersController
   def create
     vote(application_id_param, !!params[:vote_yes])
+    # vote(application_id_param, parsed_vote_value) # TODO Linda
 
     application = Application.find(application_id_param)
     redirect_to members_application_path(application)
@@ -19,6 +20,13 @@ class Members::VotesController < Members::MembersController
   end
 
   private
+
+  def parsed_vote_value
+    return Vote::YES if params[:vote_yes] == true
+    return Vote::NO if params[:vote_no] == true
+    return Vote::NOT_SURE if params[:vote_not_sure] == true
+    raise "Invalid vote value #{params.inspect}"
+  end
 
   def application_id_param
     params.require(:vote).require(:application_id)
